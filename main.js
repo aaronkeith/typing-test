7d7a5560d77208e18b4ca0fca5fb07ed3592d7d8
aaronkeith <aflisik@gmail.com>
2017-06-12 10:05:28 -0700

    User can now complete a single test and view their score. #7

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
-
 var $textExampleDisplay = document.querySelector('#textExampleDisplay')
 
 var textExampleDisplay = 'An escalator can never break: it can only become stairs.'
 
-// var charArray = textExampleDisplay.split('')
-
-function selectChar(char) {
-  var $indyChar = document.createElement('span')
-  $indyChar.textContent = char
-  return $indyChar
-}
+  function selectChar(char) {
+    var $indyChar = document.createElement('span')
+    $indyChar.textContent = char
+    return $indyChar
+  }
 
 for (var i = 0; i < textExampleDisplay.length; i++) {
   var $char = selectChar(textExampleDisplay[i])
   $textExampleDisplay.appendChild($char)
 }
 
-var $current  = document.querySelector('span')
+var $current = document.querySelector('span')
 $current.classList.add('current')
+
+var $correctCount = 0
+var $incorrectCount = 0
+var $correctDisplay = document.querySelector('#correct-display')
+var $incorrectDisplay = document.querySelector('#incorrect-display')
+////****change class of character color based on input and add to score****////
+window.addEventListener('keypress', function(event){
+  var $current = document.querySelector('span.current')
+  var $next = $current.nextElementSibling
+  var $activeChar = $current.innerText
+  if ($activeChar === event.key) {
+    $current.classList.remove('current')
+    $current.classList.add('correct')
+    $correctCount++
+  } else {
+    $current.classList.remove('current')
+    $current.classList.add('wrong')
+    $incorrectCount++
+  }
+  $correctDisplay.textContent = $correctCount
+  $incorrectDisplay.textContent = $incorrectCount
+  if ($next === null) {
+    return }
+  $next.classList.add('current')
+})
